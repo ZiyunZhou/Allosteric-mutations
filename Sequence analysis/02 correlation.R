d3a38801c824cb662d615fedf057730f18c5993f
ZiyunZhou <95513476+ZiyunZhou@users.noreply.github.com>
2022-01-27 21:00:18 +0800

    Update and rename correlation.R to 02 correlation.R

--- Sequence analysis/02 correlation.R
+++ Sequence analysis/02 correlation.R
@@ -1,15 +1,13 @@
-setwd("C:/Users/Tracy/Desktop/ALPL/保守性与共演化性计算")
+
 entropy<-read.table("entropy.txt",header = F)
 consurf<-read.table("consurf-grades.txt",header = T)$SCORE
 co_evo<-read.table("TNAP_MI-mean.txt",header = F)
 
-#计算相关性
 cor(entropy,consurf,method = "spearman")
 #-0.9126477
 cor(entropy,co_evo,method = "pearson")
 #0.9291851
 
-#比较不同致病性的单位点突变的保守性&共演化性
 library(openxlsx)
 mutsites<-read.xlsx("../mutations-stastics.xlsx",sheet=2)
 mutsites_fmt2<-data.frame(
@@ -18,7 +16,6 @@ mutsites_fmt2<-data.frame(
   former=substr(gsub("\\d", "", mutsites$mutation),1,3),
   later=substr(gsub("\\d", "",mutsites$mutation),4,6)
 )
-#将保守性&共演化性与table相匹配
 mutsites_fmt2[,5]<-entropy[mutsites_fmt2$site,]
 mutsites_fmt2[,6]<-co_evo[mutsites_fmt2$site,]
 colnames(mutsites_fmt2)[5:6]<-c("entropy","co_evolution")
@@ -26,16 +23,12 @@ mutsites_fmt2$type <- factor(mutsites_fmt2$type, levels = c("mild","severe","hSN
 
 mild_entropy<-subset(mutsites_fmt2)
 
-#作图，比较不同组别之间的差异
 library(ggplot2)
 library(ggpubr)
 
-my_comparisons<-list(
-  c("mild","severe"),c("severe","hSNP/asymptomatic"),
-  c("mild","hSNP/asymptomatic"))
+my_comparisons<-list(c("mild","severe"), c("severe","hSNP/asymptomatic"), c("mild","hSNP/asymptomatic"))
 
-ggviolin(mutsites_fmt2, x="type", y="entropy", fill = "type", 
-         add = "boxplot", add.params = list(fill="white"))+ 
+ggviolin(mutsites_fmt2, x="type", y="entropy", fill = "type", add = "boxplot", add.params = list(fill="white"))+ 
   stat_compare_means(comparisons = my_comparisons)+ 
   xlab("")+
   ylab("Entropy")+
@@ -44,10 +37,8 @@ ggviolin(mutsites_fmt2, x="type", y="entropy", fill = "type",
         axis.text.y =element_text(size = 15),
         axis.title.y = element_text(size = 20))
 
-
-ggviolin(mutsites_fmt2, x="type", y="co_evolution", fill = "type", 
-         add = "boxplot", add.params = list(fill="white"))+ 
-  stat_compare_means(comparisons = my_comparisons)+#label这里表示选择显著性标记（星号） 
+ggviolin(mutsites_fmt2, x="type", y="co_evolution", fill = "type", add = "boxplot", add.params = list(fill="white"))+ 
+  stat_compare_means(comparisons = my_comparisons)+
   xlab("")+
   ylab("Co-evolution")+
   guides(fill=FALSE)+
@@ -55,14 +46,3 @@ ggviolin(mutsites_fmt2, x="type", y="co_evolution", fill = "type",
         axis.text.y =element_text(size = 15),
         axis.title.y = element_text(size = 20))
 
-#entropy 下四分位数：0.5777158
-quantile(mutsites_fmt2$entropy)
-#0%       25%       50%       75%      100% 
-#0.0000000 0.5777158 1.4042284 2.0246949 2.5064723 
-
-#co-evolution 下四分位数：0.09164122
-quantile(mutsites_fmt2$co_evolution)
-#        0%        25%        50%        75%       100% 
-#0.00000000 0.09164122 0.21416985 0.32462309 0.44935115 
-
-write.xlsx(mutsites_fmt2,"mutation_format2.xlsx")
